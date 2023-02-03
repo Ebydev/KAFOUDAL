29b689359a1073968b2fe66cbc417effba65761b
Ebydev <brouyaoeric1@gmail.com>
2023-02-03 17:24:08 +0000

    Ajout du template Admin and Site

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+    #[Route('/article', name: 'article_home')]
+    public function article(): Response
+    {
+        return $this->render('home/article.html.twig');
+    }
+}
